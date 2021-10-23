8a2c2770ce176950e6f538372c7bdd3a6c2ee4c1
Bao <baolh106@gmail.com>
2021-10-23 17:28:28 +0700

    new code

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const supriseForm = document.querySelector('.suprise');
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 const displayMessage = function (message) {
    document.querySelector('.message').textContent = message;
+};
+const closeForm = function () {
+   score = 20;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+   displayMessage('START GUESSING!');
+   document.querySelector('.score span').textContent = score;
+   document.querySelector('body').style =
+      'background: linear-gradient(68.66deg, #24ad49 0%, #e0da3c 100%)';
+   document.querySelector('.secret-number span').textContent = '?';
+   document.querySelector('input').value = '';
+   supriseForm.classList.add('hidden');
 };
 //When you check
 document.querySelector('.check').addEventListener('click', function () {
@@ -16,9 +28,10 @@ document.querySelector('.check').addEventListener('click', function () {
    //When you win
    else if (guess === secretNumber) {
       displayMessage('Correct Number!');
-      document.querySelector('body').style =
-         'background: linear-gradient(68.66deg, #24AD49 0%, #E0DA3C 100%)';
       document.querySelector('.secret-number span').textContent = secretNumber;
+      document.querySelector('body').style =
+         'background: linear-gradient(68.66deg, #1A2A6C 0%, #B21F1F 51.04%, #FDBB2D 100%)';
+      supriseForm.classList.remove('hidden');
       if (score > highScore) {
          highScore = score;
          document.querySelector('.highscore span').textContent = highScore;
@@ -37,13 +50,7 @@ document.querySelector('.check').addEventListener('click', function () {
    }
 });
 
-document.querySelector('.btn-again').addEventListener('click', function () {
-   score = 20;
-   secretNumber = Math.trunc(Math.random() * 20) + 1;
-   displayMessage('START GUESSING!');
-   document.querySelector('.score span').textContent = score;
-   document.querySelector('body').style =
-      'background: linear-gradient(68.66deg, #1A2A6C 0%, #B21F1F 51.04%, #FDBB2D 100%)';
-   document.querySelector('.secret-number span').textContent = '?';
-   document.querySelector('input').value = '';
-});
+document.querySelector('.btn-again').addEventListener('click', closeForm);
+
+//Back to Start Screen
+document.querySelector('.suprise span').addEventListener('click', closeForm);
